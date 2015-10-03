96b7f4d267e0c42667b595ea0297a6d2c89bb60f
Christian Davis <ChristianD@csu.fullerton.edu>
2015-10-02 17:41:12 -0700

    Initial commit from seed

---
+++ README.md
@@ -0,0 +1,9 @@
+# Robo Feeder
+
+Started from the [Angular Socket.io Seed](https://github.com/btford/angular-socket-io-seed).
+
+## How to use
+
+Runs on a Raspberry Pi. 
+
+
---
+++ routes/index.js
@@ -0,0 +1,17 @@
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index');
+};
+
+exports.views = function(req, res){
+  var name = req.params.name;
+  res.render(name);
+};
+
+exports.partials = function (req, res) {
+  var name = req.params.name;
+  res.render('partials/' + name);
+};
